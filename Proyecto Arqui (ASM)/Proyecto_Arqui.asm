43ae45dbb98020f73efa5f8ee8e948c76cd1dd00
gzuni <gzuni@LAPTOP-UNDDJ82B>
2025-10-21 17:25:24 -0600

    Add project files.

---
+++ Proyecto Arqui (ASM)/Proyecto_Arqui.asm
@@ -0,0 +1,30 @@
+;Aca se encontrara el programa ensamblador del proyecto de Arqui
+;El programa recibe un numero primo y busca sus raices primitivas, este las mostrara despues.
+
+includelib \Windows\System32\kernel132.dll
+
+ExitProcess proto
+.data
+	p dd ?
+	r dw ?
+
+.code
+main PROC
+
+mainENP
+
+
+verificar_repetido PROC
+
+verificar_repetidoENP
+
+recorrer_potencias PROC
+
+recorrer_potenciasENP
+
+raices_primitivas PROC
+
+raices_primitivasENP
+
+
+

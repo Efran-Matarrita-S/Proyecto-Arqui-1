40270bb350bd8fc7d3a20eac07f6827c19011cb9
Gao? <gzunigad2007@gmail.com>
2025-10-22 17:51:07 -0600

    Incluye el while en la funcion de raices primitivas (aun sin el contenido esta nada mas el esqueleto) Falta: Todo

--- Proyecto Arqui (ASM)/Proyecto_Arqui.asm
+++ Proyecto Arqui (ASM)/Proyecto_Arqui.asm
@@ -5,7 +5,7 @@ includelib \Windows\System32\kernel132.dll
 
 ExitProcess proto
 .data
-	p dw ? ;Esto recibira el numero primo a sacarle raices primitivas (numero maximo = 32 749)
+	p dw 5 ;Esto recibira el numero primo a sacarle raices primitivas (numero maximo = 32 749)
 	r dw  0,0,0,0,0,0,0,0,0,0 ;Aca se guardaran las respuestas, este es el vector, son 10 elementos de 2 bytees (word) cada uno
 
 .code
@@ -24,7 +24,23 @@ recorrer_potencias PROC
 
 recorrer_potenciasENDP
 
-raices_primitivas PROC
+raices_primitivas PROC ;Función principal: busca todas las raíces primitivas de un número primo p
+
+	sub rsp, 8        ; reservar espacio para contador
+	mov qword ptr [RSP], 0  ; contador = 0
+
+	loop_inicio:
+	CMP word ptr [RSP], [RDI]  ;En nuestro caso compararemos n con p
+	JE fin_bucle
+	; cuerpo del bucle
+	; definiremos un registro donde quedara el verdadero o falso que devuelve la funcion, o en el stack frame no se
+
+	INC qword ptr [RSP] ;Incrementamos en 1 el contador
+	JMP loop_inicio
+	fin_bucle:
+
+	add rsp, 8 ;Eliminamos contador
+	ret        ;Retornamos
 
 raices_primitivasENDP
 
